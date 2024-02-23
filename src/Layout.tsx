12ac8128778d8d7e7389a3718606ce3e24ba7af9
서재원 <cinos81@nhnlink.co.kr>
2024-02-23 14:33:19 +0900

    진행상황

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,10 +5,34 @@ const Layout = () => {
     <>
         <ul>
             <li>
-                <Link to="case0">Case 0</Link>
+                <Link to="case0">ts + fetch</Link>
+            </li>
+            <li>
+                <Link to="case1">ts + fetch + react-router-dom</Link>
+            </li>
+            <li>
+                <Link to="case2">ts + fetch + jotai + loadable</Link>
+            </li>
+            <li>
+                <Link to="case3">ts + fetch + jotai + suspense</Link>
+            </li>
+            <li>
+                <Link to="case4">ts + fetch + jotai + suspense + react-router-dom + atomFamily</Link>
+            </li>
+            <li>
+                <Link to="case5">ts + fetch + jotai + suspense + react-router-dom + atom in atom</Link>
+            </li>
+            <li>
+                <Link to="case6">ts + fetch + react-query + loadable + react-router-dom</Link>
+            </li>
+            <li>
+                <Link to="case7">ts + fetch + react-query + suspense + react-router-dom</Link>
+            </li>
+            <li>
+                <Link to="case8">ts + fetch + react-query + suspense + react-router-dom + jotai + atom in atom</Link>
             </li>
         </ul>
-        <Outlet />
+        <Outlet/>
     </>
     )
 }
