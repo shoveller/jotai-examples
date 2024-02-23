b141f8a074f83901d067c4e796378c150e2356de
서재원 <cinos81@nhnlink.co.kr>
2024-02-23 15:21:40 +0900

    case0

--- src/Case0.tsx
+++ src/Case0.tsx
@@ -1,9 +1,38 @@
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { PokeData, getList } from "./api";
 
+type ListProps = {
+    pokeData?: PokeData
+}
+
+const List: FC<ListProps> = ({ pokeData }) => {
+    return (
+        <ul>
+        {pokeData?.results.map((item) => {
+          return <li key={item.name}>{item.name}</li>;
+        })}
+      </ul>
+    )
+}
+
+type PaginatorProps = {
+    page: number;
+    setPage: (page: number) => void
+}
+
+const Paginator: FC<PaginatorProps> = ({ page, setPage }) => {
+    return (
+        <>
+            <button onClick={() => setPage(page - 1)}>-</button>
+            <span>{page}</span>
+            <button onClick={() => setPage(page + 1)}>+</button>
+        </>
+    )
+}
+
 const Case0 = () => {
-    // 타입스크립트는 결과를 보고 원인을 파악할 수 있다 === 추론
-    // 타입스크립트는 원인을 보고 결과를 파악할 수 있다 === 추론
+  // 타입스크립트는 결과를 보고 원인을 파악할 수 있다 === 추론
+  // 타입스크립트는 원인을 보고 결과를 파악할 수 있다 === 추론
   const [page, setPage] = useState(0);  
   // 어떤 그릇인가?
   const [pokeData, setPokeData] = useState<PokeData>();
@@ -15,14 +44,8 @@ const Case0 = () => {
 
   return (
     <>
-      <ul>
-        {pokeData?.results.map((item) => {
-          return <li key={item.name}>{item.name}</li>;
-        })}
-      </ul>
-      <button onClick={() => setPage(page - 1)}>-</button>
-      <span>{page}</span>
-      <button onClick={() => setPage(page + 1)}>+</button>
+      <List pokeData={pokeData} />
+      <Paginator page={page} setPage={setPage} />
     </>
   );
 };
